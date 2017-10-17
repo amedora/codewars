502c8e0865bcc887a4823eedafb744612aaa0ac3
amedora <americandragsterracing@gmail.com>
2017-10-17 10:24:08 +0900

    add a solution for "Your Order, Please!"

---
+++ Test/6kyu/YourOrderPlease.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace codewars
+{
+    [TestFixture]
+    public class YourOrderPlease
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.AreEqual("Thi1s is2 3a T4est", Kata.Order("is2 Thi1s T4est 3a"));
+            Assert.AreEqual("Fo1r the2 g3ood 4of th5e pe6ople", Kata.Order("4of Fo1r pe6ople g3ood th5e the2"));
+            Assert.AreEqual("", Kata.Order(""));
+        }
+    }
+}
---
+++ codewars/6kyu/YourOrderPlease.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace codewars
+{
+    // https://www.codewars.com/kata/your-order-please/csharp
+    public partial class Kata
+    {
+        public static string Order(string words)
+        {
+            if (string.IsNullOrEmpty(words))
+                return "";
+
+            return string.Join(" ", words
+                .Split()
+                .OrderBy(s => Regex.Replace(s, "[^0-9]", "")));
+        }
+    }
+}
